96acd6f4e9697d0f5b4966d4da01e403707df163
Eduardo Castillo <hipoclorino@hotmail.com>
2020-10-07 14:44:34 -0500

    feat:iniatial commit initial commit

---
+++ SpecFlowProject1/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,45 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SpecFlowProject1.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            Logger.Logger.Log($"Given {number}");
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            Logger.Logger.Log($"Given {number}");
+
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            Logger.Logger.Log($"When");
+
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            Logger.Logger.Log($"Then {result}");
+
+        }
+    }
+}
